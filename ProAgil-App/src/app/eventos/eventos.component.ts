c32c85658a8cd8c8fadd36b28a39e2b3efa5938e
felipe <fpsantos86@hotmail.com>
2021-04-12 23:50:01 -0300

    Angular - Interpolação, Diretivas e Binding (V1)

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,28 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: '.NET Core',
-      Local: 'São Pauloasd'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular e .NET Core',
-      Local: 'Rio de Janeiro'
-    }
-  ];
+  _filtroLista: string = '';
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
+
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string){
+
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase()
+      .indexOf(filtrarPor) !== -1);
+
+  }
+
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem
   }
 
   getEventos(){
