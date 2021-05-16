32d3ef96e231b07725a9c7b9d36072df0bec19aa
felipe <fpsantos86@hotmail.com>
2021-05-16 20:26:20 -0300

    Ajustes no Projeto WebAPI

---
+++ ProAgil.WebAPI/Data/ProAgilContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProAgil.WebApi.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base (options)
+        {
+            
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+
+    }
+}
