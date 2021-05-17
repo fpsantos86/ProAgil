abec2b853fa2315cc9638eda4eda55447e85ab78
felipe <fpsantos86@hotmail.com>
2021-05-16 21:35:48 -0300

    Respositório - Classe - Parte 03

---
+++ ProAgil.Repository/IProAgilRepository.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using ProAgil.Domain;
+
+namespace ProAgil.Repository
+{
+    public interface IProAgilRepository
+    {
+        //GERAL
+         void Add<T>(T entity)  where T: class;
+         void Update<T>(T entity)  where T: class;
+         void Delete<T>(T entity)  where T: class;
+         Task<bool> SaveChangesAsync();
+
+         //EVENTOS
+         Task<Evento[]> GetAllEventosAsyncByTema(string tema, bool includePalestrantes);
+         Task<Evento[]> GetAllEventosAsync( bool includePalestrantes);
+         Task<Evento> GetAllEventoAsyncById(int eventoId, bool includePalestrantes);
+
+         //PALESTRANTES
+         Task<Palestrante[]> GetAllPalestrantesAsyncByName(string name, bool includeEventos);
+         Task<Palestrante[]> GetAllPalestrantesAsync( bool includeEventos);
+         Task<Palestrante> GetAllPalestranteAsyncById(int palestranteId, bool includeEventos);
+         
+    }
+}
