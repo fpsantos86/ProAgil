8fa00d67efb5907f1fdccc1a121a3a0399fb4fd1
felipe <fpsantos86@hotmail.com>
2021-05-22 17:32:32 -0300

    Implementando o componente Modal

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../_models/Evento';
 import { EventoService } from '../_services/evento.service';
 
@@ -10,56 +11,65 @@ import { EventoService } from '../_services/evento.service';
 })
 export class EventosComponent implements OnInit {
 
-  _filtroLista: string = '';
-
-  get filtroLista(): string {
-    return this._filtroLista;
-  }
-
-  set filtroLista(value: string){
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
   eventosFiltrados: Evento[] = [];
-
   eventos: Evento[] = [];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
+  modalRef: BsModalRef = new BsModalRef();
+
+  _filtroLista = '';
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService)
+    {
+
+    }
+
+    get filtroLista(): string {
+      return this._filtroLista;
+    }
+
+    set filtroLista(value: string){
+      this._filtroLista = value;
+      this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+    }
+
+    openModal(template: TemplateRef<any>){
+      this.modalRef = this.modalService.show(template);
+    }
 
 
-  constructor(private eventoService: EventoService ) { }
+    ngOnInit() {
+      this.getEventos();
+    }
 
-  ngOnInit() {
-    this.getEventos();
-  }
+    filtrarEventos(filtrarPor: string):Evento[]{
 
-  filtrarEventos(filtrarPor: string):Evento[]{
+      filtrarPor = filtrarPor.toLocaleLowerCase();
 
-    filtrarPor = filtrarPor.toLocaleLowerCase();
+      return this.eventos.filter(
+        evento => evento.tema.toLocaleLowerCase()
+        .indexOf(filtrarPor) !== -1);
 
-    return this.eventos.filter(
-      evento => evento.tema.toLocaleLowerCase()
-      .indexOf(filtrarPor) !== -1);
-
-  }
-
-  alternarImagem(){
-    this.mostrarImagem = !this.mostrarImagem
-  }
-
-  getEventos(){
-    this.eventoService.getAllEvento().subscribe(
-      (_eventos: Evento[]) => {
-        this.eventos = _eventos;
-        this.eventosFiltrados = this.eventos
-        console.log(_eventos);
-      },
-      error =>{
-        console.log(error);
       }
-    );
-  }
 
-}
+      alternarImagem(){
+        this.mostrarImagem = !this.mostrarImagem
+      }
+
+      getEventos(){
+        this.eventoService.getAllEvento().subscribe(
+          (_eventos: Evento[]) => {
+            this.eventos = _eventos;
+            this.eventosFiltrados = this.eventos
+            console.log(_eventos);
+          },
+          error =>{
+            console.log(error);
+          }
+          );
+        }
+
+      }
